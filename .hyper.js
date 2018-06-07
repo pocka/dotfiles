652a40b00ca5d358b53728b7c73963bbb7f12dd1
pocka <pocka.moe@gmail.com>
2018-06-07 18:05:58 +0900

    :art: Change theme for Hyper

--- .hyper.js
+++ .hyper.js
@@ -135,7 +135,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-pokemon","hypercwd"],
+  plugins: ["hyperterm-atom-dark","hypercwd"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
